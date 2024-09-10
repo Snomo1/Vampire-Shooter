8383745555e3663706e3787a67b502e836ed3f78
Swagatam Biswas <swagatambsws.biswas@gmail.com>
2024-09-11 05:08:43 +0530

    Weapon can shoot bullets now with lmb

--- sprites.py
+++ sprites.py
@@ -1,7 +1,7 @@
 import pygame
 
 from settings import *
-from math import atan2,degrees
+from math import atan2, degrees
 
 
 class Sprite(pygame.sprite.Sprite):
@@ -36,18 +36,33 @@ class Gun(pygame.sprite.Sprite):
         mouse_pos = pygame.Vector2(pygame.mouse.get_pos())
         player_pos = pygame.Vector2(Window_Width / 2, Window_Height / 2)
         self.player_direction = (mouse_pos - player_pos).normalize()
-        print(self.player_direction)
 
     def rotate_gun(self):
         angle = degrees(atan2(self.player_direction.x, self.player_direction.y)) - 90
         if self.player_direction.x > 0:
-            self.image =pygame.transform.rotozoom(self.gun_surface,angle, 1)
+            self.image = pygame.transform.rotozoom(self.gun_surface, angle, 1)
         else:
-            print(angle)
             self.image = pygame.transform.rotozoom(self.gun_surface, abs(angle), 1)
-            self.image = pygame.transform.flip(self.image,False,True)
+            self.image = pygame.transform.flip(self.image, False, True)
 
     def update(self, _):
         self.get_direction()
         self.rotate_gun()
         self.rect.center = self.player.rect.center + self.player_direction * self.distance
+
+
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, surf, pos, direction, groups):
+        super().__init__(groups)
+        self.image = surf
+        self.rect = self.image.get_frect(center=pos)
+        self.spawn_time = pygame.time.get_ticks()
+        self.lifetime = 1000
+
+        self.direction = direction
+        self.speed = 1200
+
+    def update(self,dt):
+        self.rect.center += self.direction * self.speed * dt
+        if pygame.time.get_ticks() - self.spawn_time >= self.lifetime:
+            self.kill()
