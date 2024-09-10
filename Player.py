3ad5716fb0691f4abb093c314e2de26dd0422949
Swagatam Biswas <swagatambsws.biswas@gmail.com>
2024-09-11 03:49:30 +0530

    Added player movement animation and a weapon object

--- Player.py
+++ Player.py
@@ -4,17 +4,32 @@ from settings import *
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos, groups, collision_sprites):
         super().__init__(groups)
+        self.load_images()
+        self.state, self.frame_index = "up", 0
         self.image = pygame.image.load(
             join("resource", "Vampire survivor", "Vampire 0 setup", "images", "player", "down",
                  "0.png")).convert_alpha()
         self.rect = self.image.get_frect(center=pos)
-        self.hitbox_rect = self.rect.inflate(-60,-5)
+        self.hitbox_rect = self.rect.inflate(-60, -90)
 
-        #movement
+        # movement
         self.direction = pygame.Vector2()
         self.speed = 500
         self.collision_sprites = collision_sprites
 
+    def load_images(self):
+        self.frames = {"left": [], "right": [], "up": [], "down": []}
+        print(list(walk(join("resource", "Vampire survivor", "Vampire 0 setup", "images", "player"))))
+
+        for state in self.frames.keys():
+
+            for folder_path, sub_folders, file_names in list(
+                    walk(join("resource", "Vampire survivor", "Vampire 0 setup", "images", "player", state))):
+                if file_names:
+                    for file_name in sorted(file_names, key=lambda name: int(name.split(".")[0])):
+                        full_path = join(folder_path, file_name)
+                        surf = pygame.image.load(full_path).convert_alpha()
+                        self.frames[state].append(surf)
 
     def input(self):
         keys = pygame.key.get_pressed()
@@ -22,20 +37,20 @@ class Player(pygame.sprite.Sprite):
         self.direction.y = int(keys[pygame.K_DOWN]) - int(keys[pygame.K_UP])
         self.direction = self.direction.normalize() if self.direction else self.direction
 
-    def move(self,dt):
-        self.hitbox_rect.x += self.direction.x *self.speed * dt
+    def move(self, dt):
+        self.hitbox_rect.x += self.direction.x * self.speed * dt
         self.collision("horizontal")
         self.hitbox_rect.y += self.direction.y * self.speed * dt
         self.collision("vertical")
         self.rect.center = self.hitbox_rect.center
 
-    def collision(self,direction):
+    def collision(self, direction):
         for sprite in self.collision_sprites:
             if sprite.rect.colliderect(self.hitbox_rect):
                 if direction == "horizontal":
-                    if self.direction.x > 0 :
+                    if self.direction.x > 0:
                         self.hitbox_rect.right = sprite.rect.left
-                    if self.direction.x < 0 :
+                    if self.direction.x < 0:
                         self.hitbox_rect.left = sprite.rect.right
                 else:
                     if self.direction.y > 0:
@@ -43,6 +58,19 @@ class Player(pygame.sprite.Sprite):
                     if self.direction.y < 0:
                         self.hitbox_rect.top = sprite.rect.bottom
 
-    def update(self,dt):
+    def animate(self, dt):
+
+        # getstate
+        if self.direction.x != 0:
+            self.state = "right" if self.direction.x > 0 else "left"
+        if self.direction.y != 0:
+            self.state = "down" if self.direction.y > 0 else "up"
+
+        # animate
+        self.frame_index = self.frame_index + 5 * dt if self.direction else 0
+        self.image = self.frames[self.state][int(self.frame_index) % len(self.frames[self.state])]
+
+    def update(self, dt):
         self.input()
         self.move(dt)
+        self.animate(dt)
--- sprites.py
+++ sprites.py
@@ -1,15 +1,53 @@
+import pygame
+
 from settings import *
+from math import atan2,degrees
+
 
 class Sprite(pygame.sprite.Sprite):
-    def __init__(self,pos,surf,groups):
+    def __init__(self, pos, surf, groups):
+        super().__init__(groups)
+        self.image = surf
+        self.rect = self.image.get_frect(topleft=pos)
+        self.ground = True
+
+
+class CollisionSprite(pygame.sprite.Sprite):
+    def __init__(self, pos, surf, groups):
         super().__init__(groups)
         self.image = surf
         self.rect = self.image.get_frect(topleft=pos)
 
 
+class Gun(pygame.sprite.Sprite):
+    def __init__(self, player, groups):
+        self.player = player
+        self.distance = 140
+        self.player_direction = pygame.Vector2(0, 1)
 
-class CollisionSprite(pygame.sprite.Sprite):
-    def __init__(self,pos,surf,groups):
+        # sprite_setup
         super().__init__(groups)
-        self.image = surf
-        self.rect = self.image.get_frect(topleft = pos)
+        self.gun_surface = pygame.image.load(
+            join("resource", "Vampire survivor", "Vampire 0 setup", "images", "gun", "gun.png")).convert_alpha()
+        self.image = self.gun_surface
+        self.rect = self.image.get_frect(center=self.player.rect.center + self.player_direction * self.distance)
+
+    def get_direction(self):
+        mouse_pos = pygame.Vector2(pygame.mouse.get_pos())
+        player_pos = pygame.Vector2(Window_Width / 2, Window_Height / 2)
+        self.player_direction = (mouse_pos - player_pos).normalize()
+        print(self.player_direction)
+
+    def rotate_gun(self):
+        angle = degrees(atan2(self.player_direction.x, self.player_direction.y)) - 90
+        if self.player_direction.x > 0:
+            self.image =pygame.transform.rotozoom(self.gun_surface,angle, 1)
+        else:
+            print(angle)
+            self.image = pygame.transform.rotozoom(self.gun_surface, abs(angle), 1)
+            self.image = pygame.transform.flip(self.image,False,True)
+
+    def update(self, _):
+        self.get_direction()
+        self.rotate_gun()
+        self.rect.center = self.player.rect.center + self.player_direction * self.distance
